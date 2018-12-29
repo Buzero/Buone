c792c81ffc544075d0211411e3829ff79004448b
Buzero <1271301097@qq.com>
2018-12-28 17:54:16 -0800

    add a line

--- Test.java
+++ Test.java
@@ -1,5 +1,9 @@
 public class Test{
 	public static void main(String[] args){
-		System.out.println("heloo git");
+		System.out.println("heloo git\n");
+		System.out.println("next line\n");
+
+
+
 	}
 }
